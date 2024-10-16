8a41da16f7012da9106474e1b88f76e36e21a769
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-10-16 12:34:34 +0600

    mongoos quries landing pages

---
+++ db/queries.js
@@ -0,0 +1,11 @@
+import { eventModel } from "@/models/event-models";
+
+async function getAllEvents() {
+    const allEvents = await eventModel.find().lean();
+    return (allEvents);
+}
+
+export {
+    getAllEvents
+};
+
