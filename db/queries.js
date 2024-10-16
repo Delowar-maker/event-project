b104909e3184205168e0b98aeda6765df9301236
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-10-16 13:31:27 +0600

    replecemongoIdInObject

--- db/queries.js
+++ db/queries.js
@@ -6,7 +6,12 @@ async function getAllEvents() {
     return replaceMongoIdInArray(allEvents);
 }
 
+async function getEventById(eventId) {
+    const event = await eventModel.findById(eventId).lean();
+    return replaceMongoIdInObject(event);
+}
 export {
-    getAllEvents
+    getAllEvents,
+    getEventById
 };
 
